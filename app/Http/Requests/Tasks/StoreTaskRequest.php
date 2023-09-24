69b0b10432a806990fad1650fb2d3ec84b1c408d
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2023-09-24 21:13:33 +0900

    Add: commonize a FormRequest and create new requests for update a task

--- app/Http/Requests/Tasks/StoreTaskRequest.php
+++ app/Http/Requests/Tasks/StoreTaskRequest.php
@@ -1,19 +1,11 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Tasks;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\BaseRequest;
 
-class StoreTaskRequest extends FormRequest
+class StoreTaskRequest extends BaseRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
